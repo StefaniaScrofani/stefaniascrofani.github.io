4a0b153a3c990dbfd5440edf89f73742d40da885
StefaniaScrofani <68299470+StefaniaScrofani@users.noreply.github.com>
2025-05-23 08:08:33 +0000

    Deploying to gh-pages from @ StefaniaScrofani/stefaniascrofani.github.io@623350af5e5b4f909e1a6289faf4f28d20c3b1fb 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -58,35 +58,28 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%73%74%65%66%61%6E%69%61.%73%63%72%6F%66%61%6E%69%30%32@%67%6D%61%69%6C.%63%6F%6D", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://www.linkedin.com/in/https://www.linkedin.com/in/stefania-scrofani/", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-orcid',
+        title: 'ORCID',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
+          window.open("https://orcid.org/https://orcid.org/0009-0008-9092-9767", "_blank");
         },
       },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
         handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://scholar.google.com/citations?user=https://scholar.google.com/citations?user=jJOfVKsAAAAJ&hl=en", "_blank");
         },
       },{
       id: 'light-theme',
