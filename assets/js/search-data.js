607eec0143c495a53f9e3d3e3edab7f15596a504
StefaniaScrofani <68299470+StefaniaScrofani@users.noreply.github.com>
2025-07-01 13:25:07 +0000

    Deploying to gh-pages from @ StefaniaScrofani/stefaniascrofani.github.io@73e3c85da5d10c553e64b398a77a7254b2f5c227 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -30,7 +30,10 @@ ninja.data = [{
           handler: () => {
             window.location.href = "/projects/";
           },
-        },{id: "news-easter-egg-in-csdid",
+        },{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
+          title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
+          description: "",
+          section: "News",},{id: "news-easter-egg-in-csdid",
           title: 'Easter Egg in csdid',
           description: "",
           section: "News",handler: () => {
@@ -40,10 +43,7 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_1.html";
-            },},{id: "news-see-you-at-aom-in-copenhagen",
-          title: 'See you at AOM in Copenhagen!',
-          description: "",
-          section: "News",},{
+            },},{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
